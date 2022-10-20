52477b9b0e0500cb33fbb12c0a79128381de1c10
이준환 <niip00ng@daum.net>
2022-10-21 00:10:59 +0900

    좌측 상단 클릭시 자산 리프래시

--- muonVault/src/view/common/topComponent.tsx
+++ muonVault/src/view/common/topComponent.tsx
@@ -1,9 +1,16 @@
 import React, {useRef} from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import FastImage from 'react-native-fast-image';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import { UserApiResponse } from '~/api/interface/userApiResponse';
+import User from '~/api/User';
+import { updateScAssets } from '~/store/action/scAction';
+import { updateVaultsFromApi } from '~/store/action/VaultAction';
+import { updateWallet } from '~/store/action/walletAction';
 import {RootState} from '~/store/modules';
 import {BASE_BACKGROUND, MAIN_BLACK} from '../ColorCode';
+import Toast from 'react-native-simple-toast';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const top_logo = require('../../../assets/image/top_logo.png');
 
@@ -11,10 +18,29 @@ type PropTopComponent = {};
 const TopComponent: React.FC<
   React.PropsWithChildren<PropTopComponent>
 > = ({}) => {
+
+  const dispatch: any = useDispatch();
   const vaultStore = useSelector((root: RootState) => root.vaultsStore);
+
+
+  function updateUserInfo() {
+    User.info()
+      .then(e => {
+        const res: UserApiResponse = e;
+
+        dispatch(updateWallet(res.Wallet));
+        dispatch(updateVaultsFromApi());
+        dispatch(updateScAssets(res.SafeAddress, res.Wallet, res.MUP));
+        Toast.show(`refresh my assets`, Toast.SHORT);
+      })
+  }
+
 
   return (
     <View style={s.topComponentWrapper}>
+      <TouchableOpacity onPress={()=>{
+        updateUserInfo();
+      }}>
       <FastImage
         resizeMode="contain"
         style={{
@@ -23,6 +49,7 @@ const TopComponent: React.FC<
         }}
         source={top_logo}
       />
+      </TouchableOpacity>
       <Text style={{fontSize: 16, fontWeight: '700', color: MAIN_BLACK}}>
         {vaultStore.totalAssets.muon.toFixed(2)} MU:P
       </Text>
