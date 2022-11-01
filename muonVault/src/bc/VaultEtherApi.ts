7577a38f7e1055d0948202c5041865bbc8cca530
이준환 <niip00ng@daum.net>
2022-11-01 23:49:01 +0900

    nonce 값 확인용

--- muonVault/src/bc/VaultEtherApi.ts
+++ muonVault/src/bc/VaultEtherApi.ts
@@ -48,6 +48,7 @@ const requestEtherWithdrawConfirm = async (
     signer,
   );
 
+  const nonce = await provider.getTransactionCount(wallet.address);
   const gasPriceNumber = await provider.getGasPrice();
   const gasLimit = await contract.estimateGas.requestAndConfirmWithdraw(
     to,
@@ -69,6 +70,7 @@ const requestEtherWithdrawConfirm = async (
   console.log('필요한가스', Number(gasLimitNumber*Number(gasPriceNumber)));
   console.log('전송할이더', parseToWei(Number(value)));
   console.log('필요한총액', totalNeedValue);
+  console.log('출금요청 nonce', nonce);
   
   if(Number(parseInt(etherBalance._hex, 16)) <  Number(gasLimitNumber*Number(gasPriceNumber))){
     return INSUFFICIENT_FUNDS+","+parseWeiToEther( Number(gasLimitNumber*Number(gasPriceNumber)));
